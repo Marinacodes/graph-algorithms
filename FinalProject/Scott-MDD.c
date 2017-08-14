d780a230c3c22d69ae2cf55c9a5dea900f6e4284
Marina Scott <marina_scott@outlook.com>
2017-08-13 20:21:42 -0700

    Attempted MDD bounding

---
+++ FinalProject/Scott-MDD.c
@@ -0,0 +1,382 @@
+/*
+ * This program attempts to find a minimum dominating set of a graph using MDD bounding. 
+ * It does not check the input graph for correctness. 
+ * Input: provided through standard input 
+ * 		  Format: number of vertices, degree of each vertex followed by a list of its neighbours
+ * Output: goes out to standard output
+ * 		   If verbosity is set to 1, graph followed by a dominating set
+ *         If verbosity is set to 0, graph number, number of vertices, dominating set order
+ */
+
+#include <stdio.h> 
+#include <stdlib.h>
+
+// maximum number of vertices
+#define NMAX 2187
+// maximum degree
+#define DEG_MAX 16
+#define DEBUG 0
+
+int read_graph(int* n, int degree[NMAX], int G[NMAX][DEG_MAX]); 
+void print_graph(int n, int degree[NMAX], int G[NMAX][DEG_MAX]);
+void min_dom_set(int level, int n, int degree[NMAX], int G[NMAX][DEG_MAX], int* n_dominated, int num_dominated[NMAX],
+				 int num_choice[NMAX], int* size, int dom[NMAX], int* min_size, int min_dom[NMAX], int mdd);
+void print_vector(int size, int arr[]);
+void print_verbose_min_dom_set(int min_size, int n, int min_dom[]);
+void print_nonverbose_min_dom_set(int graph_num, int n, int min_size);
+int calculate_mdd(int n, int degree[NMAX], int dom_degree[NMAX], int max_dom_degree[NMAX], int G[NMAX][DEG_MAX], int num_dominated[NMAX]);
+
+int main(int argc, char *argv[])
+{
+	int n; // number of vertices
+	int degree[NMAX]; // degrees of vertices
+	int G[NMAX][DEG_MAX]; // adjacency list
+	int n_dominated; // number of dominated vertices
+	int num_dominated[NMAX]; // number of times a vertex is dominated
+	int num_choice[NMAX]; // number of times a vertex could be dominated if all white vertices were red
+	int size; // number of vertices added to dominating set
+	int dom[NMAX]; // vertices added to dominating set
+	int min_size; // minimum number of vertices in dominating set
+	int min_dom[NMAX]; // vertices added to minimum dominating set
+	int verbose; // user-specified verbosity level, 0 for terse, 1 for verbose
+	int graph_num = 0; // graph number
+	int mdd = 0; // mdd bound
+    
+	// prints error message if incorrect usage
+	if (argc < 2)
+	{
+		printf("Must specify verbosity level (0 for terse, 1 for verbose)\nExample: a.out 0 < in.txt > out.txt\n");
+		return EXIT_FAILURE;
+	}
+
+    // reads in the user-specified verbosity level
+	verbose = atoi(argv[1]);
+	
+	// processes input    
+    while (read_graph(&n, degree, G) == 0)
+    {
+		// increments the number of graph
+		graph_num++;
+
+        // if verbose output selected, print graph, find and print minimum dominating set
+		if (verbose == 1)
+		{
+			print_graph(n, degree, G);
+			min_dom_set(0, n, degree, G, &n_dominated, num_dominated, num_choice, &size, dom, &min_size, min_dom, mdd);
+			print_verbose_min_dom_set(min_size, n, min_dom);
+		}
+		// if terse output selected, print graph number and number of vertices, find and print minimum dominating set order
+        else
+        {
+			min_dom_set(0, n, degree, G, &n_dominated, num_dominated, num_choice, &size, dom, &min_size, min_dom, mdd);
+			print_nonverbose_min_dom_set(graph_num, n, min_size);
+        }
+    }
+	
+	return EXIT_SUCCESS;
+}
+
+/*
+ * Calcuates MDD bounding
+ */
+int calculate_mdd(int n, int degree[NMAX], int dom_degree[NMAX], int max_dom_degree[NMAX], int G[NMAX][DEG_MAX], int num_dominated[NMAX])
+{
+	int i, j, temp, mdd;
+
+	for (i = 0; i < n; i++)
+	{
+		dom_degree[i] = 0;
+        max_dom_degree[i] = 0;
+	}
+	
+    // populate dom_degree with number of undominated vertices in the
+    // closed neighbourhood of i
+    for (i = 0; i < n; i++) 
+    {
+        for (j = 0; j <= degree[i]; j++)
+        {
+            if (num_dominated[G[i][j]] == 0) 
+            {
+                dom_degree[i]++;
+            }
+        }
+    }
+    
+    // for each undominated vertex i, max_dom_degree[i]
+    // is equal to the maximum dom_degree of an undominated neighbour
+    for (i = 0; i < n; i++)
+    {
+		for (j = 0; j <= degree[i]; j++)
+		{
+			if (num_dominated[i] == 0)
+			{
+				if (num_dominated[G[i][j]] == 0)
+				{
+					if (dom_degree[G[i][j]] > max_dom_degree[i])
+					{
+						max_dom_degree[i] = dom_degree[G[i][j]]; // largest number of vertices a neighbour can dominate
+					}
+				}
+			}
+		}
+    }
+
+    // bubble sort max_dom_degree in ascending order
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0 ; j < n - i - 1; j++)
+        {
+            if (max_dom_degree[j] > max_dom_degree[j + 1]) 
+            {
+                temp = max_dom_degree[j];
+                max_dom_degree[j] = max_dom_degree[j + 1];
+                max_dom_degree[j + 1] = temp;
+            }
+        }
+    }
+
+    i = 0;
+    mdd = 0;
+
+    while (i < n)
+    {		
+        if (max_dom_degree[i] != 0) 
+		{
+        	i += max_dom_degree[i];
+        	mdd++;
+		}
+		else 
+		{
+			i++;
+		}
+    }
+    return mdd;
+}
+
+/*
+ * Reads in a graph from standard input
+ */
+int read_graph(int* n, int degree[NMAX], int G[NMAX][DEG_MAX]) 
+{
+    // reads in input as long as end of file is not reached
+	if (fscanf(stdin, "%d", n) == 1)
+	{
+		int i, j;
+
+		// for each vertex
+		for (i = 0; i < *n; i++)
+		{
+			// read in its degree
+			if (scanf("%d", &degree[i]) != 1)
+			{
+				return EXIT_FAILURE;
+			}
+
+			if (degree[i] >= DEG_MAX)
+			{
+				printf("A vertex degree cannot be equal to or greater than %d", DEG_MAX);
+				return EXIT_FAILURE;
+			}
+
+			// for each degree
+			for (j = 0; j < degree[i]; j++)
+			{
+				// read in the vertex's neighbours
+				if (scanf("%d", &G[i][j]) != 1)
+				{
+					return EXIT_FAILURE;
+				}
+			}
+		}
+        return EXIT_SUCCESS;
+	}
+    return EXIT_FAILURE;
+}
+
+/*
+ * Prints graph in verbose output mode
+ */
+void print_graph(int n, int degree[NMAX], int G[NMAX][DEG_MAX])
+{
+	int i, j;
+	
+	printf("%d\n", n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", degree[i]);
+		for (j = 0; j < degree[i]; j++)
+		{			
+			printf("%d ", G[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+/*
+ * Finds a minimum dominating set of a graph
+ * Coluring: red = in dominating set, blue = excluded from dominating set, white = undecided
+ */
+void min_dom_set(int level, int n, int degree[NMAX], int G[NMAX][DEG_MAX], int* n_dominated, int num_dominated[NMAX],
+				 int num_choice[NMAX], int* size, int dom[NMAX], int* min_size, int min_dom[NMAX], int mdd)
+{
+	int i;
+    int dom_degree[NMAX]; // how many vertices can be dominated
+    int max_dom_degree[NMAX]; // largest number of vertices a neighbour can dominate
+
+	// initialize data structures
+	// level is the vertex currently under consideration
+	if (level == 0)
+	{
+		*n_dominated = 0;		
+		*size = 0;
+		// no values assigned to dom[]
+		*min_size = n;		
+		
+		for (i = 0; i < n; i++)
+		{
+			num_choice[i] = degree[i] + 1;
+			num_dominated[i] = 0;
+			min_dom[i] = i;
+			// add vertex to list of own neighbours
+			G[i][degree[i]] = i;			
+		}		 
+	}
+
+	#if DEBUG
+	printf("\nLevel %3d: \n", level);
+	printf("Current dominating set:\n");
+	print_vector(*size, dom);
+	printf("Minimum dominating set:\n");
+	print_vector(*min_size, min_dom);
+	printf("Number of vertices dominated: %3d\n", *n_dominated);
+	printf("Number of choices per vertex:\n");
+	print_vector(n, num_choice);
+	printf("Number of times dominated:\n");
+	print_vector(n, num_dominated);
+	printf("Size is: %d, min_size is: %d\n", *size, *min_size);
+	#endif
+	
+	// backtrack
+	for (i = 0; i < n; i++)
+	{
+		if (num_choice[i] == 0)
+		{
+			return;
+		}
+	}
+
+	int u = n - *n_dominated; // number of undominated vertices
+    
+	mdd = calculate_mdd(n, degree, dom_degree, max_dom_degree, G, num_dominated);
+    
+    if (*size + mdd >= *min_size)
+	{
+	 	return;
+	}
+
+	if (level == n || *n_dominated == n)
+	{
+		if (*size < *min_size)
+		{
+			for (i = 0; i < n; i++)
+			{
+				min_dom[i] = dom[i];
+			}
+			*min_size = *size;
+		}
+		return;
+	}
+
+	// exhaustive backtrack
+	u = level;
+	
+	// try colouring a vertex blue
+	for (i = 0; i <= degree[u]; i++)
+	{
+		num_choice[G[u][i]]--;
+	}
+
+	// recursive call
+	min_dom_set(level + 1, n, degree, G, n_dominated, num_dominated, num_choice, size, dom, min_size, min_dom, mdd);
+
+	// undo colouring a vertex blue (colour it white)
+	for (i = 0; i <= degree[u]; i++)
+	{
+		num_choice[G[u][i]]++;
+	}
+	
+	// try colouring a vertex red
+	dom[u] = 1;
+	(*size)++;
+
+	for (i = 0; i <= degree[u]; i++)
+	{
+		num_dominated[G[u][i]]++;
+		// increase number of dominated vertices
+		if (num_dominated[G[u][i]] == 1)
+		{
+			(*n_dominated)++;
+		}
+	}
+	
+	// recursive call
+	min_dom_set(level + 1, n, degree, G, n_dominated, num_dominated, num_choice, size, dom, min_size, min_dom, mdd);
+
+	// undo colouring a vertex red (colour it white)
+	dom[u] = 0;
+	(*size)--;	
+
+	for (i = 0; i <= degree[u]; i++)
+	{
+		num_dominated[G[u][i]]--;
+		// decrease number of dominated vertices
+		if (num_dominated[G[u][i]] == 0)
+		{
+			(*n_dominated)--;
+		}
+	}
+}
+
+/*
+ * Prints contents of an array in debug mode
+ */
+void print_vector(int size, int arr[])
+{
+	int i;
+	for (i = 0; i < size; i++)
+	{
+		printf("%d: %d\n", i, arr[i]);
+	}
+	printf("\n");
+}
+
+/*
+ * Prints a minimum dominating set order followed by a minimum dominating set in verbose output mode
+ */
+void print_verbose_min_dom_set(int min_size, int n, int min_dom[])
+{
+	printf("%d\n", min_size);
+
+    int i;
+    for (i = 0; i < n; i++) 
+	{
+        if (min_dom[i] == 1) 
+		{
+            printf("%d ", i);
+        }
+    }
+    printf("\n\n");
+
+	fflush(stdout);
+}
+
+/*
+ * Prints graph number, number of vertices, and a minimum dominating set order in terse output mode
+ */
+void print_nonverbose_min_dom_set(int graph_num, int n, int min_size)
+{
+	printf("%5d %3d %3d\n", graph_num, n, min_size);
+	
+	fflush(stdout);
+}
